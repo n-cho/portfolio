7b092ede73b5d906bf658625d03de0cb9b9fcff1
Nathan Cho <nathanjwc06@gmail.com>
2023-05-22 20:36:57 -0700

    may 22

--- src/components/Honors.js
+++ src/components/Honors.js
@@ -9,9 +9,8 @@ export default function Honors(){
             <p className="font-light text-gray-400">Here are some of my honors and awards</p>
 
             <div className="flex flex-col md:flex-row mt-4 gap-5">
-                <HonorCard name="Robot Skills World Champions (WC)" issued="VEX Robotics" desc="Team with highest combined top Programming and top Driving Skills Challenge score (Robot Performance)." />
-                <HonorCard name="3rd Place Winner of IT Network System Administration - LKS Jawa Tengah" issued="KEMENDIKBUD RI" desc="Linux Server, Windows Server, Cisco Configuration, Cisco Troubleshooting, Network and System Programmabillity, Containerized Service." />
-                <HonorCard name="1st Place Winner of IT Network System Administration - LKS Kab. Tegal" issued="KEMENDIKBUD RI" desc="Linux Server, Cisco Configuration." />
+                <HonorCard name="Robot Skills World Champions (WC) - 2020-2021 Change Up" issued="VEX Robotics" desc="Team with highest combined top Programming and top Driving Skills Challenge score (Robot Performance)." />
+                <HonorCard name="Think Award (WC) - 2020-2021 Change Up" issued="VEX Robotics" desc="Team with impressive and effective autonomous programming." />
             </div>
             <img src={hr} className="w-full mt-8 md:h-2" alt="hr" />
         </div>
